e0193e4ad3bde98b89f6c5670f257b580137e9bd
Breno Sousa <breno.sousa@uniriotec.br>
2021-12-08 10:50:15 -0300

    Make improvements to the sign up page + API call

--- src/app/components/signup/models/signupformDTO.ts
+++ src/app/components/signup/models/signupformDTO.ts
@@ -1,13 +1,17 @@
 export interface SignUpFormDTO {
   email: string;
   username: string;
-  passwordCtrl: string;
-  passwordConfirmationCtrl: string;
-  cepCtrl: string;
-  cityCtrl: string;
-  stateCtrl: string;
-  districtCtrl: string;
-  numberCtrl: string;
-  additionalInfoCtrl: string;
-  streetCtrl: string;
+  password: string;
+  passwordConfirmation: string;
+  cep: string;
+  city: string;
+  state: string;
+  district: string;
+  number: string;
+  additionalInfo: string;
+  street: string;
+  phone1: string;
+  phone2: string;
+  phone3: string;
+  secondaryEmail: string;
 }
--- src/app/components/signup/services/signup.service.ts
+++ src/app/components/signup/services/signup.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
+import { catchError, distinctUntilChanged, Observable, take, tap } from 'rxjs';
 import { SignUpFormDTO } from '../models/signupformDTO';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { NotificationService } from '../../../../app/shared/services/notification.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private notificationService: NotificationService) { }
 
-  signUp(signUpForm: SignUpFormDTO): void {
+  signUp(signUpForm: SignUpFormDTO): Observable<boolean> {
+
+    return this.http.post<boolean>(`${environment.baseUrl}/clients/users`, signUpForm)
+      .pipe(
+        take(1),
+        tap((success: boolean) => {
+
+          if (success) {
+            this.notificationService.success('Cadastro concluído!');
+          } else {
+            this.notificationService.alert('Informações inválidas');
+          }
+
+        }),
+        distinctUntilChanged(),
+        catchError(err => {
+          this.notificationService.error('Erro ao cadastrar usuário.');
+          throw new Error(err);
+        })
+      );
 
   }
 }
