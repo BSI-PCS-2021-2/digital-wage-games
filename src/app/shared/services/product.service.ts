e14466d49045b784570accee5abb59e159994c14
Thiago Sousa <thiagosilvadesousa@edu.unirio.br>
2021-12-10 04:01:51 -0300

    Updated Catologo and Produto pages

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Product } from '../../../app/shared/models/product.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 import { Injectable } from '@angular/core';
 import { NotificationService } from './notification.service';
 
@@ -11,27 +11,11 @@ import { NotificationService } from './notification.service';
 })
 
 export class ProductService {
-  products: Product[];
 
   constructor(private http: HttpClient, private notificationService: NotificationService) { }
 
-  getProducts(): Product[] {
-    this.http.get<Product[]>(`${environment.baseUrl}/store/products`)
-                    .subscribe(
-                      products => {
-                        products.forEach(product => {
-                          let price = Number(product.price)/100;
-                          product.price = "R$ "+ price.toString().replace('.', ',');
-                          console.log(product.price);
-                        });
-                        this.products = products
-                      },
-                      error => {
-                        this.notificationService.error('Ocorreu um erro!');
-                        throw new Error(error)
-                      }
-                    );
-                    return this.products;
-  }
 
+  getProduct():Observable<Product[]>{ 
+    return this.http.get<Product[]>(`${environment.baseUrl}/store/products`);                 
+  }
 }
