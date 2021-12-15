89650ced37314ccbd4f0ffe449e7dbfcc07247c2
Breno Sousa <breno.sousa@uniriotec.br>
2021-12-15 01:29:56 -0300

    Improve catalogue page

--- src/app/components/catalogo/catalogo.component.ts
+++ src/app/components/catalogo/catalogo.component.ts
@@ -3,13 +3,16 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { MatStepperIntl } from '@angular/material/stepper';
 import { Product } from '../../../app/shared/models/product.model';
 import { ProductService } from '../../shared/services/product.service';
+import { Options } from '@angular-slider/ngx-slider';
+import { AuthenticationService } from '../../shared/services/authentication.service';
+import { NotificationService } from '../../shared/services/notification.service';
+import { Router } from '@angular/router';
 
 
 @Injectable()
 export class StepperIntl extends MatStepperIntl {
   override optionalLabel = '(OPCIONAL)';
 }
-
 
 @Component({
   selector: 'app-catalogo',
@@ -19,7 +22,26 @@ export class StepperIntl extends MatStepperIntl {
 
 export class CatalogoComponent implements OnInit {
 
-  constructor(private productService: ProductService) { }
+  minPriceValue: number = 0;
+  maxPriceValue: number = 1000;
+  priceOptions: Options = {
+    floor: 0,
+    ceil: 1000
+  };
+
+  minRatingValue: number = 0;
+  maxRatingValue: number = 1000;
+  ratingOptions: Options = {
+    floor: 0,
+    ceil: 5
+  };
+
+  constructor(
+    private productService: ProductService,
+    public authenticationService: AuthenticationService,
+    private notificationService: NotificationService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((products) => {
@@ -27,6 +49,7 @@ export class CatalogoComponent implements OnInit {
       this.convert(this.products);
     });
   }
+
   convert(products: Product[]): void {
     products.forEach(product => {
       let price = Number(product.price)/100;
@@ -34,6 +57,24 @@ export class CatalogoComponent implements OnInit {
       console.log(product.price + " 123");
     });
     this.products = products
+  }
+
+  addToCart(productId: number): void {
+    if (this.authenticationService.isLoggedIn()) {
+      this.notificationService.success('Produto adicionado ao carrinho');
+    } else {
+      this.notificationService.alert('Você precisa estar conectado');
+      this.router.navigate(['/login']);
+    }
+  }
+
+  goToCart(): void {
+    if (this.authenticationService.isLoggedIn()) {
+      this.router.navigate(['/carrinho']);
+    } else {
+      this.notificationService.alert('Você precisa estar conectado');
+      this.router.navigate(['/login']);
+    }
   }
 
   selected = 'relevant';
