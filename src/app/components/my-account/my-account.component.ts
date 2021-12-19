01c1fb2dbebf41b72d3361ca108e3570da9a09f0
pedro clain <pedroclain@edu.unirio.br>
2021-12-18 21:36:15 -0300

    Implement increase wallet credit back-end communication

--- src/app/components/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account.component.ts
@@ -3,22 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { BuyCreditsComponent } from './buy-credits/buy-credits.component';
 import { NotificationService } from 'src/app/shared/services/notification.service';
-
-
-
-export interface User {
-  id: number;
-  name: string;
-  credits: number;
-  avaliations: number;
-  games: number;
-}
-
-export interface Product {
-  cartItemId: number;
-  name: string;
-  desc: string;
-}
+import { WalletService } from 'src/app/shared/services/wallet.service';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service';
 
 @Component({
   selector: 'app-my-account',
@@ -28,51 +14,17 @@ export interface Product {
 
 export class MyAccountComponent implements OnInit {
 
-  constructor(public dialog: MatDialog,
+  constructor(private dialog: MatDialog,
               private router: ActivatedRoute,
-              private notificationService: NotificationService) { }
-
-  produto: Product[] = [{
-    cartItemId: 0,
-    name: "Dark Souls 3",
-    desc: "testeLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  },
-  {
-    cartItemId: 1,
-    name: "Dark Souls 3",
-    desc: "teste"
-  },
-  {
-    cartItemId: 2,
-    name: "Dark Souls 3",
-    desc: "teste"
-  }, {
-    cartItemId: 0,
-    name: "Dark Souls 3",
-    desc: "testeLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  },
-  {
-    cartItemId: 1,
-    name: "Dark Souls 3",
-    desc: "teste"
-  },
-  {
-    cartItemId: 2,
-    name: "Dark Souls 3",
-    desc: "teste"
-  },]
-
-usuario: User = {
-  id: 0,
-  name: "Digital Wave Games",
-  credits: 200000,
-  avaliations: 102,
-  games: 115
-}
+              private notificationService: NotificationService,
+              private walletService: WalletService,
+              private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
-    this.creditsReal = this.formatPrice(this.usuario.credits);
-    console.log(this.router.params)
+    this.username = this.authenticationService.getUsername();
+    this.walletService.getWallet(this.username).subscribe(w => {
+      this.funds = w.funds;
+    })
     if (this.router.snapshot.paramMap.get('success')) {
       this.notificationService.success("Sua compra foi realizada com sucesso!");
     }
@@ -93,9 +45,6 @@ usuario: User = {
     return (v / 100).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
   }
 
-  rateArr = [1, 2, 3, 4];
-  creditsReal: string;
-
-
-
+  funds: number;
+  username: string;
 }
