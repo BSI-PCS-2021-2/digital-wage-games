9ead889ae4d2f5f3955b458b5cff97b894b10bcc
Thiago Sousa <thiagosilvadesousa@edu.unirio.br>
2022-01-10 10:12:10 -0300

    support email added

--- src/app/components/support/support.component.ts
+++ src/app/components/support/support.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SupportEmailDTO } from '../../shared/models/dto/supportEmail/supportEmailDTO';
+import { AuthenticationService } from '../../shared/services/authentication.service'
+import { SupportService } from '../../shared/services/support.service'
 
 @Component({
   selector: 'app-support',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupportComponent implements OnInit {
 
-  constructor() { }
+  public supportFormGroup: FormGroup;
+  public supportMessageForm: FormGroup;
+
+  constructor(
+    private supportService: SupportService,
+    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.supportFormGroup = this.formBuilder.group({
+      email: "exemple@gmail.com",
+      clientEmail: [''],
+      message: [''],
+      category: ['']
+    });
   }
-
+  enviarMensagem(): void {
+    const supportForm: SupportEmailDTO = {
+      email: "exemple@gmail.com",
+      clientEmail: "exemple@gmail.com",
+      category: this.supportFormGroup.get('category').value,
+      message: this.supportFormGroup.get('message').value
+    }
+    this.supportService.sendSupportEmail(supportForm);
+  }
 }
---
+++ src/app/shared/services/support.service.ts
@@ -0,0 +1,23 @@
+import { catchError, distinctUntilChanged, Observable, take, tap } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { NotificationService } from '../../../app/shared/services/notification.service';
+import { AuthenticationService } from '../../../app/shared/services/authentication.service'
+import { SupportEmailDTO } from '../models/dto/supportEmail/supportEmailDTO'
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupportService {
+
+  constructor(
+    private http: HttpClient,
+    private notificationService: NotificationService,
+   ) { }
+
+  sendSupportEmail(supportEmailDTO: SupportEmailDTO): Observable<boolean> {
+    console.log(supportEmailDTO);
+    return this.http.post<boolean>(`${environment.baseUrl}/support/support`, supportEmailDTO);
+  }
+}
