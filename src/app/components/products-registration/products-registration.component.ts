137d8f9caa88f658ba347c47be9f2be01f077d4c
Thiago Sousa <thiagosilvadesousa@edu.unirio.br>
2022-01-23 17:01:41 -0300

    products registration sending form infos

--- src/app/components/products-registration/products-registration.component.ts
+++ src/app/components/products-registration/products-registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductRegistrationDTO } from '../../shared/models/dto/product-registration/productRegistrationDTO';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-products-registration',
@@ -7,19 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsRegistrationComponent implements OnInit {
 
-  image:string = "../../../assets/images/ImageField.png"
-  constructor() { }
+  public registrationFormGroup: FormGroup;
+
+  image: string = "../../../assets/images/ImageField.png"
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
-    document.addEventListener("keydown",function(e){
-      if(e.key==='Enter'){
+    document.addEventListener("keydown", function (e) {
+      if (e.key === 'Enter') {
         e.preventDefault();
       }
     });
+    this.registrationFormGroup = this.formBuilder.group({
+      name: ['', Validators.required],
+      price: ['',Validators.required],
+      amount: ['',Validators.required],
+      description: ['',Validators.required],
+      image: ['',Validators.required],
+      ageRating:['',Validators.required]
+    });
   }
 
-  atualizarImagem(event:any):void{
+  atualizarImagem(event: any): void {
     this.image = event.target.value;
   }
 
+  public registerProduct():void{
+    if(this.isFormValid()){
+    const registrationForm: ProductRegistrationDTO = {
+      name: this.registrationFormGroup.get('name').value,
+      price: this.registrationFormGroup.get('price').value*100,
+      description: this.registrationFormGroup.get('description').value,
+      image: this.registrationFormGroup.get('image').value,
+      ageRating: this.registrationFormGroup.get('ageRating').value,
+      amount: this.registrationFormGroup.get('amount').value
+    }
+    console.log(registrationForm);
+  }
+  }
+  private isFormValid (): boolean {
+    return this.registrationFormGroup.valid;
+  }
 }
