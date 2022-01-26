254ba4a6adc31d2cf87b0b31359b09f2a64ffd1c
pedro clain <pedroclain@edu.unirio.br>
2022-01-26 19:43:05 -0300

    general product crud refactoring

--- src/app/components/products-registration/products-registration.component.ts
+++ src/app/components/products-registration/products-registration.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductRegistrationDTO } from '../../shared/models/dto/product/productRegistrationDTO';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { ProductService } from 'src/app/shared/services/product.service';
 import { NotificationService } from 'src/app/shared/services/notification.service';
 import * as moment from 'moment';
@@ -36,7 +36,7 @@ export class ProductsRegistrationComponent implements OnInit {
       amount: ['',Validators.required],
       description: ['',Validators.required],
       image: ['',Validators.required],
-      releaseDate:['',Validators.required],
+      releaseDate: new FormControl(),
       ageRating:['',Validators.required],
       platform:['',Validators.required],
       gender:['',Validators.required],
@@ -49,24 +49,36 @@ export class ProductsRegistrationComponent implements OnInit {
   }
 
   public registerProduct(): void {
-    const dto = {
-      name: this.registrationFormGroup.controls['name'].value,
-      price: this.registrationFormGroup.controls['price'].value,
-      amount: this.registrationFormGroup.controls['amount'].value,
-      description: this.registrationFormGroup.controls['name'].value,
-      releaseDate: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
-      ratingSystemId: this.registrationFormGroup.controls['ageRating'].value,
-      platformId: this.registrationFormGroup.controls['platform'].value,
-      genderId: this.registrationFormGroup.controls['gender'].value,
-      publisherId: this.registrationFormGroup.controls['publisher'].value,
-    }
-    console.log(dto)
-    this.productService.postProduct(dto);
     if(this.isFormValid()){
-     
+      const dto = {
+        name: this.registrationFormGroup.controls['name'].value,
+        price: this.registrationFormGroup.controls['price'].value * 100,
+        amount: this.registrationFormGroup.controls['amount'].value,
+        description: this.registrationFormGroup.controls['name'].value,
+        releaseDate: moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+        ratingSystemId: this.registrationFormGroup.controls['ageRating'].value,
+        platformId: this.registrationFormGroup.controls['platform'].value,
+        genderId: this.registrationFormGroup.controls['gender'].value,
+        publisherId: this.registrationFormGroup.controls['publisher'].value,
+      }
+      this.productService.postProduct(dto);
+      this.notificationService.success('Produto adicionado com sucesso!')
+    } else {
+      this.notificationService.error('Preencha todos os campos obrigatórios.')
     }
   }
   private isFormValid (): boolean {
     return this.registrationFormGroup.valid;
+  }
+
+  clearFields() {
+        this.registrationFormGroup.controls['name'].reset();
+        this.registrationFormGroup.controls['price'].value
+        this.registrationFormGroup.controls['amount'].value
+        this.registrationFormGroup.controls['name'].value
+        this.registrationFormGroup.controls['ageRating'].value
+        this.registrationFormGroup.controls['platform'].value
+        this.registrationFormGroup.controls['gender'].value
+        this.registrationFormGroup.controls['publisher'].value
   }
 }
