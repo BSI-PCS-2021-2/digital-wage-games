65460c01fb318634ff554a6675486247825c9791
= <=>
2022-01-27 13:48:43 -0300

    Sign in with Google + Gerencianet

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,52 +4,103 @@ import { AuthenticationService } from '../../shared/services/authentication.serv
 import { Router } from '@angular/router';
 import { SignInFormDTO } from './models/signInFormDTO';
 import { NotificationService } from 'src/app/shared/services/notification.service';
+import {
+  GoogleLoginProvider,
+  SocialAuthService,
+  SocialUser,
+} from 'angularx-social-login';
+import { GoogleSignInDTO } from './models/googleSignInDTO';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   public loginFormGroup: FormGroup;
+  user: SocialUser;
+  loggedIn: boolean;
 
   constructor(
     private formBuilder: FormBuilder,
     private authenticationService: AuthenticationService,
     private notificationService: NotificationService,
-    private router: Router
-    ) { }
+    private router: Router,
+    private authService: SocialAuthService
+  ) {}
 
   ngOnInit(): void {
-
     this.loginFormGroup = this.formBuilder.group({
       usernameCtrl: [''],
-      passwordCtrl: ['']
+      passwordCtrl: [''],
+    });
+
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = user != null;
     });
   }
 
   public login(): void {
-
     const signInForm: SignInFormDTO = {
       username: this.loginFormGroup.get('usernameCtrl').value,
       password: this.loginFormGroup.get('passwordCtrl').value,
-    }
+    };
 
     if (signInForm.username !== '' && signInForm.password !== '') {
-      this.authenticationService.getAuthenticationInfo(signInForm.username).subscribe(r => {
-        if (r !== undefined && r.banned) {
-          this.notificationService.error(`Tentativas bloqueadas, espere ${(new Date(r.nextAllowedAccess).getTime() - new Date(Date.now()).getTime()) * 1000} segundos para tentar novamente.`)
-          this.loginFormGroup.disable();
-        } else {
-          this.authenticationService.login(signInForm).subscribe((response) => {
-            if (response.banned) {
-              this.notificationService.error(`Tentativas bloqueadas, espere ${(new Date(r.nextAllowedAccess).getTime() - new Date(Date.now()).getTime()) * 1000} segundos para tentar novamente.`)
-              this.loginFormGroup.disable();
-            }
-          });
-        }
-      })
+      this.authenticationService
+        .getAuthenticationInfo(signInForm.username)
+        .subscribe((r) => {
+          if (r !== undefined && r.banned) {
+            this.notificationService.error(
+              `Tentativas bloqueadas, espere ${
+                (new Date(r.nextAllowedAccess).getTime() -
+                  new Date(Date.now()).getTime()) *
+                1000
+              } segundos para tentar novamente.`
+            );
+            this.loginFormGroup.disable();
+          } else {
+            this.authenticationService
+              .login(signInForm)
+              .subscribe((response) => {
+                if (response.banned) {
+                  this.notificationService.error(
+                    `Tentativas bloqueadas, espere ${
+                      (new Date(r.nextAllowedAccess).getTime() -
+                        new Date(Date.now()).getTime()) *
+                      1000
+                    } segundos para tentar novamente.`
+                  );
+                  this.loginFormGroup.disable();
+                }
+              });
+          }
+        });
     }
+  }
+
+  loginWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(() => {
+      if (this.user.email !== '' && this.user.id !== '') {
+        const googleSignIn: GoogleSignInDTO = {
+          email: this.user.email,
+          id: this.user.id,
+          name: this.user.name,
+        };
+
+        this.authenticationService.loginWithGoogle(googleSignIn).subscribe();
+
+        this.authenticationService.setProfileAvatar(this.user.photoUrl);
+      }
+    });
+  }
+
+  refreshToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
   }
 }
--- src/app/shared/services/authentication.service.ts
+++ src/app/shared/services/authentication.service.ts
@@ -1,85 +1,128 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { catchError, distinctUntilChanged, Observable, take, tap } from "rxjs";
-import { SignInFormDTO } from "../../components/login/models/signInFormDTO";
-import { environment } from "../../../environments/environment";
-import { NotificationService } from "./notification.service";
-import { CartService } from "./cart.service";
-import * as moment from "moment";
-import { Jwt } from "../models/jwt.model";
-import { stringify } from "querystring";
-import { SignInResponse } from "../models/signInResponse.model";
-import { AuthenticationInfo } from "../models/authenticationInfo.dto";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, distinctUntilChanged, Observable, take, tap } from 'rxjs';
+import { SignInFormDTO } from '../../components/login/models/signInFormDTO';
+import { environment } from '../../../environments/environment';
+import { NotificationService } from './notification.service';
+import { CartService } from './cart.service';
+import * as moment from 'moment';
+import { Jwt } from '../models/jwt.model';
+import { stringify } from 'querystring';
+import { SignInResponse } from '../models/signInResponse.model';
+import { AuthenticationInfo } from '../models/authenticationInfo.dto';
+import { GoogleSignInDTO } from 'src/app/components/login/models/googleSignInDTO';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class AuthenticationService {
-
   constructor(
     private http: HttpClient,
     private notificationService: NotificationService,
     private router: Router,
     private cartService: CartService
-    ) { }
+  ) {}
 
   public login(signInForm: SignInFormDTO): Observable<SignInResponse> {
-
-    return this.http.post<SignInResponse>(`${environment.baseUrl}/auth/authenticate`, signInForm)
+    return this.http
+      .post<SignInResponse>(
+        `${environment.baseUrl}/auth/authenticate`,
+        signInForm
+      )
       .pipe(
         take(1),
         tap((response: SignInResponse) => {
           if (response.jwt !== undefined) {
-            console.log(response)
+            console.log(response);
             this.setSession(response.jwt);
             this.notificationService.success('Você está autenticado!');
             this.router.navigate(['/']);
           } else {
-            this.notificationService.alert('Nome de usuário ou senha inválidos');
+            this.notificationService.alert(
+              'Nome de usuário ou senha inválidos'
+            );
           }
-
         }),
         distinctUntilChanged(),
-        catchError(err => {
+        catchError((err) => {
           this.notificationService.error('Erro ao autenticar usuário.');
           throw new Error(err);
         })
       );
-
   }
 
-  public getAuthenticationInfo(username: string): Observable<AuthenticationInfo> {
-    return this.http.get<AuthenticationInfo>(`${environment.baseUrl}/clients/user/${username}`);
+  public loginWithGoogle(
+    googleSignIn: GoogleSignInDTO
+  ): Observable<SignInResponse> {
+    return this.http
+      .post<SignInResponse>(
+        `${environment.baseUrl}/auth/authenticate-with-google`,
+        googleSignIn
+      )
+      .pipe(
+        take(1),
+        tap((response: SignInResponse) => {
+          if (response.jwt !== undefined) {
+            console.log(response);
+            this.setSession(response.jwt);
+            this.notificationService.success('Você está autenticado!');
+            this.router.navigate(['/']);
+          } else {
+            this.notificationService.alert(
+              'Nome de usuário ou senha inválidos'
+            );
+          }
+        }),
+        distinctUntilChanged(),
+        catchError((err) => {
+          this.notificationService.error('Erro ao autenticar usuário.');
+          throw new Error(err);
+        })
+      );
+  }
+
+  public getAuthenticationInfo(
+    username: string
+  ): Observable<AuthenticationInfo> {
+    return this.http.get<AuthenticationInfo>(
+      `${environment.baseUrl}/clients/user/${username}`
+    );
   }
 
   private setSession(jwt: Jwt) {
-    const expiresAt = moment().add(jwt.expiresIn,'second');
+    const expiresAt = moment().add(jwt.expiresIn, 'second');
     const username = jwt.username;
     console.log(jwt.userId);
     this.cartService.getCartByClient(jwt.userId).subscribe((cart) => {
-      localStorage.setItem("cart_id", `${cart.id}`);
+      localStorage.setItem('cart_id', `${cart.id}`);
     });
-    localStorage.setItem("user_id", `${jwt.userId}`);
+    localStorage.setItem('user_id', `${jwt.userId}`);
     localStorage.setItem('id_token', jwt.idToken);
-    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
-    localStorage.setItem("username", username);
+    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+    localStorage.setItem('username', username);
+  }
 
+  public setProfileAvatar(avatar: string) {
+    localStorage.setItem('user_avatar', avatar);
   }
 
   public logout() {
-    localStorage.removeItem("id_token");
-    localStorage.removeItem("expires_at");
-    localStorage.removeItem("username");
-    localStorage.removeItem("cart_id");
-    localStorage.removeItem("user_id");
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    localStorage.removeItem('username');
+    localStorage.removeItem('cart_id');
+    localStorage.removeItem('user_id');
+    localStorage.removeItem('user_avatar');
     this.notificationService.alert('Você está desconectado!');
     this.router.navigate(['/']);
   }
 
   public isLoggedIn(): boolean {
-    return moment().isBefore(this.getExpiration()) && localStorage.getItem("expires_at") !== null;
+    return (
+      moment().isBefore(this.getExpiration()) &&
+      localStorage.getItem('expires_at') !== null
+    );
   }
 
   public isLoggedOut(): boolean {
@@ -87,7 +130,7 @@ export class AuthenticationService {
   }
 
   public getExpiration() {
-    const expiration = localStorage.getItem("expires_at");
+    const expiration = localStorage.getItem('expires_at');
     const expiresAt = JSON.parse(expiration);
     return moment(expiresAt);
   }
@@ -104,4 +147,7 @@ export class AuthenticationService {
     return localStorage.getItem('user_id');
   }
 
+  public getAvatar() {
+    return localStorage.getItem('user_avatar');
+  }
 }
